5a379687532aa5a0f657fe566f5a0f373183af4f
nabilfaturr <108581394+nabilfaturr@users.noreply.github.com>
2025-10-29 20:33:45 +0700

    feat(web): add layout and define header

--- apps/web/src/components/shared/layout.provider.tsx
+++ apps/web/src/components/shared/layout.provider.tsx
@@ -1,4 +1,8 @@
 import { cn } from "@/lib/utils";
+import { Heading } from "@/components/shared/heading";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { ThemeToggle } from "./theme.toggle";
 
 type LayoutProviderProps = {
   children: React.ReactNode;
@@ -7,13 +11,24 @@ type LayoutProviderProps = {
 
 export function LayoutProvider({ children, className }: LayoutProviderProps) {
   return (
-    <main
-      className={cn(
-        `font-sans max-w-3xl min-h-screen flex items-center justify-center mx-auto`,
-        className
-      )}
-    >
-      {children}
-    </main>
+    <div className="min-h-screen max-w-2xl mx-auto border font-sans">
+      <header className="flex justify-between items-center p-5 border-b">
+        <Heading level={2} variant="mono">Reclara</Heading>
+        <div className="flex justify-between items-center gap-2">
+          <Button asChild>
+            <Link href={"/auth/sign-up"}>Sign Up</Link>
+          </Button>
+          <ThemeToggle />
+        </div>
+      </header>
+      <main
+        className={cn(
+          `font-sans max-w-3xl min-h-screen flex items-center justify-center mx-auto`,
+          className
+        )}
+      >
+        {children}
+      </main>
+    </div>
   );
 }
---
+++ apps/web/src/features/summary/components/summary.page.tsx
@@ -0,0 +1,5 @@
+import { LayoutProvider } from "@/components/shared/layout.provider";
+
+export function SummaryPage() {
+  return <LayoutProvider>Summary Page</LayoutProvider>;
+}
