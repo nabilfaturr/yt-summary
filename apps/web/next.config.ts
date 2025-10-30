2ca3c5b2f43c78d0726e249750a61aea8da607c3
nabilfaturr <108581394+nabilfaturr@users.noreply.github.com>
2025-10-30 18:26:27 +0700

    feat(web): add load env from monorepo

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
+import { config } from "dotenv";
+import path from "path";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// load from the monorepo root env first
+config({
+  path: path.resolve(__dirname, "../../.env.local"),
+});
+
+// then load from the app specific env to override
+config({
+  path: path.resolve(__dirname, ".env.local"),
+});
+
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
